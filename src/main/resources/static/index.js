0f94bdb532623fdff7dfa6213c863b19845dce07
emduf6392 <143519040+emduf6392@users.noreply.github.com>
2024-04-24 14:07:05 +0200

    Lage mulighet for å endre billett

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -102,10 +102,11 @@ function getTicket(){
 }
 function formatTicket(tickets){
     let out = "<table class='table table-striped'><tr>" +
-        "<th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>E-post</th><th></th></tr>"
+        "<th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>E-post</th><th></th><th></th></tr>"
     for (const ticket of tickets){
         out += "<tr><td>"+ticket.movie+"</td><td>"+ticket.number+"</td><td>"+ticket.firstName+"</td>" +
         "<td>"+ticket.lastName+"</td><td>"+ticket.phoneNb+"</td><td>"+ticket.email+"</td>" +
+            "<td><a class='btn btn-primary' href='changes.html?id="+ticket.id+"'>Endre</a></td>"+
             "<td><button class='btn btn-danger' onclick='deleteOne("+ticket.id+")'>Slett</button></td></tr>"
     }
     out += "</table>"
