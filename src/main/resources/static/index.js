81c009510603d578cbe7561d3a1d201f7e407af8
emduf6392 <143519040+emduf6392@users.noreply.github.com>
2024-04-24 13:31:36 +0200

    ferdig oblig

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -102,13 +102,20 @@ function getTicket(){
 }
 function formatTicket(tickets){
     let out = "<table class='table table-striped'><tr>" +
-        "<th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>E-post</th></tr>"
+        "<th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>E-post</th><th></th></tr>"
     for (const ticket of tickets){
         out += "<tr><td>"+ticket.movie+"</td><td>"+ticket.number+"</td><td>"+ticket.firstName+"</td>" +
-        "<td>"+ticket.lastName+"</td><td>"+ticket.phoneNb+"</td><td>"+ticket.email+"</td></tr>"
+        "<td>"+ticket.lastName+"</td><td>"+ticket.phoneNb+"</td><td>"+ticket.email+"</td>" +
+            "<td><button class='btn btn-danger' onclick='deleteOne("+ticket.id+")'>Slett</button></td></tr>"
     }
     out += "</table>"
     $("#allTickets").html(out);
+}
+function deleteOne(id) {
+    const url = "/deleteOne?id="+id;
+    $.get(url, function(){
+        getTicket();
+    })
 }
 function deleteTickets(){
     $.get("/deleteTicket", function (){
