7ae91a0b95705633404276bd40ea12354f4a6d26
emduf6392 <143519040+emduf6392@users.noreply.github.com>
2024-04-24 14:53:06 +0200

    javascript endre billett

--- src/main/resources/static/changes.js
+++ src/main/resources/static/changes.js
@@ -1,4 +1,29 @@
 $(function (){
     const id = window.location.search.substring(1);
-    const url
-})
+    const url = "/getOne?"+id;
+    $.get(url, function (ticket){
+        $("#id").val(ticket.id);
+        $("#movie").val(ticket.movie);
+        $("#number").val(ticket.number);
+        $("#firstName").val(ticket.firstName);
+        $("#lastName").val(ticket.lastName);
+        $("#phoneNb").val(ticket.phoneNb);
+        $("#email").val(ticket.email);
+    });
+});
+
+function changeTicket() {
+    const ticket = {
+        movie: $("#chosenMovie").val(),
+        number: $("#number").val(),
+        firstName: $("#firstName").val(),
+        lastName: $("#lastName").val(),
+        phoneNb: $("#phoneNb").val(),
+        email: $("#email").val()
+    };
+
+    $.post("/changeTicket", ticket, function () {
+        window.location.href = "/index.html";
+    });
+}
+
