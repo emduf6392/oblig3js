a96c4ea3a546eaeb07b2bd2500cdd5293f2875ef
emduf6392 <143519040+emduf6392@users.noreply.github.com>
2024-04-24 15:37:08 +0200

    ferdig endring

--- src/main/resources/static/changes.js
+++ src/main/resources/static/changes.js
@@ -1,19 +1,38 @@
 $(function (){
-    const id = window.location.search.substring(1);
-    const url = "/getOne?"+id;
-    $.get(url, function (ticket){
-        $("#id").val(ticket.id);
-        $("#movie").val(ticket.movie);
-        $("#number").val(ticket.number);
-        $("#firstName").val(ticket.firstName);
-        $("#lastName").val(ticket.lastName);
-        $("#phoneNb").val(ticket.phoneNb);
-        $("#email").val(ticket.email);
-    });
+    getMovie();
+    getOneTicket();
 });
+
+function getMovie(){
+    $.get("/getMovie",function(movies){
+        formatMovies(movies);
+    });
+}
+function formatMovies(movies){
+    let out = "<select id='chosenMovie' required><option label='Velg film her'></option>"
+    for (const movie of movies){
+        out += "<option>"+movie.movie+"</option>"
+    }
+    out += "</select>"
+    $("#movie").html(out);
+}
+function getOneTicket() {
+    const id = window.location.search.substring(1);
+    const url = "/getOne?" +id;
+    $.get(url, function (oneTicket){
+        $("#id").val(oneTicket.id);
+        $("#chosenMovie").val(oneTicket.movie);
+        $("#number").val(oneTicket.number);
+        $("#firstName").val(oneTicket.firstName);
+        $("#lastName").val(oneTicket.lastName);
+        $("#phoneNb").val(oneTicket.phoneNb);
+        $("#email").val(oneTicket.email);
+    });
+}
 
 function changeTicket() {
     const ticket = {
+        id : $("#id").val(),
         movie: $("#chosenMovie").val(),
         number: $("#number").val(),
         firstName: $("#firstName").val(),
@@ -23,7 +42,7 @@ function changeTicket() {
     };
 
     $.post("/changeTicket", ticket, function () {
-        window.location.href = "/index.html";
+        window.location.href = "index.html";
     });
 }
 
